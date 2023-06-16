5bac983cac4c1161a805dbbf5f2135454eceb38d
lizabespalova <bespalovalizv@gmail.com>
2023-06-17 01:19:21 +0300

    Господи помилуй, дайте мне масика, я согласна готовить, лишь бы не эта прога, аааааааа, помилуй гспд с этой джавооооой бляяяя

---
+++ Student-Portal/src/main/java/com/studentportal/StudentPortal/Helpbot/service/command/callbackquerycommands/BotHasQueryCommand.java
@@ -0,0 +1,6 @@
+package com.studentportal.StudentPortal.Helpbot.service.command.callbackquerycommands;
+
+import com.studentportal.StudentPortal.Helpbot.service.command.Command;
+
+public interface BotHasQueryCommand extends Command {
+}
